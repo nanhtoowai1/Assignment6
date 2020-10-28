425020561bb839237caed9726cad91cea3296b7a
nanhtoowai1 <nanhtoowai1@gmail.com>
2020-10-28 22:04:45 +0630

    Assignment6

--- App.java
+++ App.java
@@ -1,12 +1,23 @@
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		int inc=10,dec=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter row number: ");
+		int row=sc.nextInt();
 		
-		for(int i=1;i<=5;i++) {
+		System.out.print("Enter column number: ");
+		int col=sc.nextInt();
+		
+		sc.close();
+		
+		int inc=col,dec=1;
+		
+		for(int i=1;i<=row;i++) {
 			int count=1;
-			for(int j=1;j<=9;j++) {
-				if(j<inc && j>dec) {
+			for(int j=1;j<=col;j++) {
+				if(j<=inc && j>=dec) {
 					System.out.print(count);
 					count++;
 				}
